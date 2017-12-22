1c96bda95ddb49350b4c04cf851c8c55dd791a50
Ashwin <ashu271989@gmail.com>
2017-12-22 11:07:31 +0530

    Api Testing Tutorials

--- src/test/java/com/apitesting/ErrorFilterInRestAssured.java
+++ src/test/java/com/apitesting/ErrorFilterInRestAssured.java
@@ -1,54 +1,45 @@
 package com.apitesting;
+
 import static org.hamcrest.Matchers.notNullValue;
+
 import java.io.PrintStream;
 import java.io.StringWriter;
+
 import org.apache.commons.io.output.WriterOutputStream;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 import io.restassured.RestAssured;
+import io.restassured.filter.log.ErrorLoggingFilter;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 
-public class FiltersInRestAssured {
-
-	public  StringWriter requestWriter;
-	public  PrintStream  requestCapture;
+public class ErrorFilterInRestAssured {
 	
-	public  StringWriter responseWriter;
-	public  PrintStream  responseCapture;
 	
-
+	public StringWriter errorWriter;
+	public PrintStream errorCapture;
 	
 	@BeforeClass
 	  public void setBaseUri () {
 
 	    RestAssured.baseURI = "https://maps.googleapis.com";
-	    requestWriter = new StringWriter();
-		requestCapture =  new PrintStream(new WriterOutputStream(requestWriter),true);
-	    
-	    responseWriter = new StringWriter();
-	    responseCapture = new PrintStream(new WriterOutputStream(responseWriter),true);
-
-	  
-
+        errorWriter = new StringWriter();
+        errorCapture = new PrintStream(new WriterOutputStream(errorWriter));
 		
 	  }
 	
-	
-	  
-	  @Test
+	 @Test
 	  public void singleAssertion()  {
 		 
 		 RestAssured.given ()
-		 .filter(new RequestLoggingFilter(requestCapture))
-		 .filter(new ResponseLoggingFilter(responseCapture))
+		 .filter(new ErrorLoggingFilter(errorCapture))
 	    .param ("query", "Churchgate Station")
 	    .param ("key", "AIzaSyBrhdZP1wWpMXVEvzpY4-3W-FKieCYhVXg")
 	    .when()
-	    .get ("/maps/api/place/textsearch/json").
+	    .get ("/maps/api/place/textsearchs/json").
 	     then()
-	    .contentType(ContentType.JSON)
 	    .root("results[0].geometry.viewport.northeast")
 	    .body("lat",
 	    		notNullValue(),"lng",notNullValue())
@@ -56,10 +47,9 @@ public class FiltersInRestAssured {
 	   .body("lat",
 	    		notNullValue(),"lng",notNullValue());
 	   
-	   System.out.println(requestWriter.toString());
-	   System.out.println(responseWriter.toString());
+	   System.out.println(errorWriter.toString());
 
 	  }	 
 	
-	
+
 }
