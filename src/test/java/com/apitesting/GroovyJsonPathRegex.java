00f20f98c826a21e4a4e318771aad6d1c680b723
Ashwin <ashu271989@gmail.com>
2017-12-23 12:21:02 +0530

    API Testing Tutorial - Json Path With Regex

--- src/test/java/com/apitesting/GroovyJsonPathRegex.java
+++ src/test/java/com/apitesting/GroovyJsonPathRegex.java
@@ -17,16 +17,34 @@ public class GroovyJsonPathRegex {
 		RestAssured.baseURI = "http://api.walmartlabs.com/";
 	}
 
+	
+	//The msrp values of items whose name starts with iPod 
 	@Test
-	public void findAnItemWithSalePrice() {
-		List<Float> names = given().queryParam("query", "ipod").queryParam("format", "json")
+	public void findAnItemWhoseNameStartsWithiPod() {
+		
+		List<Float> msrps = given().queryParam("query", "ipod").queryParam("format", "json")
 				.queryParam("apiKey", "bcu25etp7zbd6nn6hveuq3pg").when().get("v1/search").then().extract()
 				.path("items.findAll{it.name==~/iPod.*/}.msrp");
-		// path();
-		// List<String> name
-		for (Float name : names)
+		
+		for (Float msrp : msrps)
 
-			System.out.println("The msrp values of items whose name starts with iPod " + name);
+			System.out.println("The msrp values of items whose name starts with iPod " + msrp);
 	}
+	
+	//The salePrice values of items whose name ends with Player 
+	
+	@Test
+	public void findAnItemWhoseNameEndsWithPlayer() {
+		
+		List<Float> salePrices = given().queryParam("query", "ipod").queryParam("format", "json")
+				.queryParam("apiKey", "bcu25etp7zbd6nn6hveuq3pg").when().get("v1/search").then().extract()
+				.path("items.findAll{it.name==~/.*Player/}.salePrice");
+		
+		for (Float salePrice : salePrices)
+
+			System.out.println("The salePrice values of items whose name ends with Player " + salePrice);
+	}
+	
+	
 
 }
